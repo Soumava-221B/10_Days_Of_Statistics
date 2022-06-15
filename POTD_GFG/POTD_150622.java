a5d37f52b1aa0a0b9f7f9eb71ce095841ce122a7
Soumava-221B <soumavadas06@gmail.com>
2022-06-15 17:56:01 +0530

    POTD Basic

---
+++ POTD_GFG/POTD_150622.java
@@ -0,0 +1,63 @@
+
+import java.io.*;
+import java.util.*;
+
+
+class Array {
+    
+    // Driver code
+	public static void main (String[] args) throws IOException{
+		// Taking input using buffered reader
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int testcases = Integer.parseInt(br.readLine());
+		
+		// looping through all testcases
+		while(testcases-- > 0){
+		    String line = br.readLine();
+		    String[] element = line.trim().split("\\s+");
+		    int sizeOfArray = Integer.parseInt(element[0]);
+		     
+		    int arr [] = new int[sizeOfArray];
+		    
+		    line = br.readLine();
+		    String[] elements = line.trim().split("\\s+");
+		    for(int i = 0;i<sizeOfArray;i++){
+		        arr[i] = Integer.parseInt(elements[i]);
+		    }
+		    Complete obj = new Complete();
+		    int ans = obj.chocolates(arr, sizeOfArray);
+		    System.out.println(ans);
+		}
+	}
+}
+
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Complete{
+    
+   
+    // Function for finding maximum and value pair
+    public static int chocolates (int arr[], int n) {
+        
+        int min = Integer.MAX_VALUE;
+       
+       for(int i= n-1; i>=0; i--){
+           if(arr[i]<min){
+               min= arr[i];
+           }
+           
+       }
+       return min;
+    }
+    
+    
+}
+
+
