0d8f1e56ff4ad42186b222f7dccf9da08e0846bb
Soumava-221B <soumavadas06@gmail.com>
2022-06-22 08:14:22 +0530

    POTD Medium

---
+++ POTD_GFG/POTD_220622.java
@@ -0,0 +1,49 @@
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+	public static void main (String[] args) {
+		
+		Scanner sc = new Scanner (System.in);
+		int t = Integer.parseInt(sc.next());
+		
+		while(t>0)
+		{
+		    String s = sc.next();
+		    
+		    Solution T = new Solution();
+		    System.out.println(T.fun(s));
+		    
+		    t--;
+		}
+	}
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    public int fun(String str)
+    {
+        int a = 0;
+   int ab = 0;
+   int abc = 0;
+
+   for (int i = 0; i < str.length(); i++) {
+     char ch = str.charAt(i);
+
+     if (ch == 'a') {
+       a = ((2*a%1000000007)%1000000007 + 1)%1000000007;
+     } else if (ch == 'b') {
+       ab = ((2 * ab%1000000007)%1000000007 + a%1000000007)%1000000007;
+     } else if (ch == 'c') {
+       abc = ((2 * abc%1000000007)%1000000007 + ab%1000000007)%1000000007;
+     }
+   }
+   return abc%1000000007;
+   }
+    }
